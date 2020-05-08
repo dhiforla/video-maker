6e5ce009472152137c11c9478cd0378413353e49
dhiforlan <65019879+dhiforla@users.noreply.github.com>
2020-05-08 12:02:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,22 +2,24 @@ const readline = require ('readline-sync')
 
 function start(){
   const content = {}
-  
+
   content.searchTerm = askAndReturnSearchTerm()
-  content.prefix = askAndReturnPreFix()
-  
+  content.prefix = askAndReturnPrefix()
+
   function askAndReturnSearchTerm(){
     return readline.question('Type a Wikipedia search term: ')
-  }
-  
+ }
+
   function askAndReturnPreFix(){
     const prefixes = ['Who is', 'What is', 'The Historia of']
     const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
-    
-    console.log(selectedPrefixIndex)
+    const selectedPrefixText = prefixes[selectedPrefixIndex]
+
+    return (selectedPrefixText)
+
   }
-  
+
    console.log(content)
  }
- 
+
  start()
